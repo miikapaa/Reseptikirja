0e2657148128d3dd9f7440fe3c0b683fe49866db
Natalia Edelmann <natalia.edelmann@elo.fi>
2020-04-05 18:37:47 +0300

    lisätty omat filet tabeille

---
+++ R/Reseptihaku.R
@@ -0,0 +1,13 @@
+
+# Define UI for application that draws a histogram
+ReseptihakuUI <- function(id){ 
+  ns <- NS(id)
+  fluidPage(
+    h5("Resepti")
+  )
+}
+# Define server logic required to draw a histogram
+ReseptihakuServer <- function(input, output, session) {
+  
+  
+}
